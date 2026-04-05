4bb69da5145ce1527928eb6bbaa75ff3e304acaa
nitesh095 <anshukumargautam095@gmail.com>
2026-04-05 19:08:28 +0530

    guess-number-game completed, working on architecture update

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
 	welcomeAndInfoMessage()
@@ -8,7 +11,7 @@ func main() {
 
 	for {
 		_, err := fmt.Scanln(&choice)
-		if err != nil {
+		if err != nil || choice < 1 || choice > 3 {
 			fmt.Println("Error reading input. Please enter a valid number.")
 			continue
 		}
@@ -22,9 +25,50 @@ func main() {
 	}
 
 	fmt.Printf("You have selected %s difficulty. You will have %d attempts to guess the number.\n", level, attempts)
+
+	randomNumber := generateRandomNumber()
+
+	gameCore(attempts, randomNumber)
 }
 
-/// getDifficultyRange returns the difficulty level and the number of attempts based on the user's choice.
+/// gameCore is the main game loop that handles user guesses and provides feedback until the user either guesses correctly or exhausts all attempts.
+func gameCore(attempts int, randomNumber int) {
+	var guess int
+	guessCorrectly := false
+
+	for i := 0; i < attempts; i++ {
+		fmt.Printf("Attempt %d: Enter your guess: ", i+1)
+		_, err := fmt.Scanln(&guess)
+		if err != nil {
+			fmt.Println("Error reading input. Please enter a valid number.")
+			i--
+			continue
+		}
+
+		if guess == randomNumber {
+			fmt.Println("Congratulations! You've guessed the number correctly!")
+			guessCorrectly = true
+			break
+		} else if guess < randomNumber {
+			fmt.Println("Too low! Try again.")
+		} else {
+			fmt.Println("Too high! Try again.")
+		}
+	}
+
+	if !guessCorrectly {
+		fmt.Println("Sorry, you've used all your attempts!")
+		fmt.Printf("Game Over! The correct number was %d.\n", randomNumber)
+	}
+}
+
+// / generateRandomNumber generates a random number between 1 and 100
+func generateRandomNumber() int {
+	randomNumber := rand.Intn(100) + 1
+	return randomNumber
+}
+
+// / getDifficultyRange returns the difficulty level and the number of attempts based on the user's choice.
 func getDifficultyRange(choice int) (string, int) {
 	switch choice {
 	case 1:
@@ -41,7 +85,7 @@ func getDifficultyRange(choice int) (string, int) {
 	}
 }
 
-/// welcomeAndInfoMessage displays the welcome message and game information to the user.
+// / welcomeAndInfoMessage displays the welcome message and game information to the user.
 func welcomeAndInfoMessage() {
 
 	fmt.Println("Welcome to Number Guessing Game 🚀")
